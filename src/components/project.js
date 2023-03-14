e78750d8c3d0954ca14ffa37088929f5730b8b22
Hereus <74949222+hereusmizo@users.noreply.github.com>
2023-03-14 13:50:27 +0000

    Cashfree

--- src/components/project.js
+++ src/components/project.js
@@ -1,7 +1,7 @@
 const project = {
   name: "COLLES",
-  address1: "Zarkawt, MacDonald Hill",
-  address2: "Aizawl, Mizoram - 796007",
+  address1: "Kanan Veng, RK Road",
+  address2: "Aizawl, Mizoram - 796009",
   email: "collesmizo@gmail.com",
   contact: "+91 9612602728",
 };
